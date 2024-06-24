6b6bc50436eedfde8476b31890dc698508eb11dc
g30Webbrain <uiqboljon24@gmail.com>
2024-06-24 18:03:51 +0500

    Test fayl yaratildi

---
+++ review.js
@@ -0,0 +1,29 @@
+// GITHUB
+// GIT
+
+
+// Windows -> GitBash
+// MacOs -> Terminal
+// Linux -> Terminal
+
+
+// ========================
+
+// pwd -> Qayerda turganimizni ko`rsatadi
+// cd -> change directory, biror papkaga kirish
+// ls -> turgan papkamiz ichida nimalar borligini ko`rsatadi
+
+
+// CTRL + L yoki clear -> Terminalni tozalaydi.
+
+
+// git init -> git initilazation -> Ushbu papkamizda git bilan ishlashni taminlaydi
+
+
+// git config --list -> git konfiguratsiyasini korish
+// git config user.name g30Webbrain -> Git dagi username ni g30Webbrainga ozgartirish
+// git config user.email example@gmail.com -> Git dagi email ni example@gmail.com ozgartirish
+
+// git remote add [anyname] [link] -> git repo ni ulash
+// git remote -v -> qaysi repolar ulangaligini tekshirish
+
